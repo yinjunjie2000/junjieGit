e4f01a1b2566b234bb7c49ab15eda8e3237a111d
tt <y_junjie@sohu.com>
2022-02-19 09:53:07 +0800

    first commit

---
+++ app/src/main/java/com/android/tv/junjiegit/MainActivity.java
@@ -0,0 +1,22 @@
+package com.android.tv.junjiegit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+/*
+ * Main Activity class that loads {@link MainFragment}.
+ */
+public class MainActivity extends FragmentActivity {
+  //this is 0 firt base ;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_browse_fragment, new MainFragment())
+                    .commitNow();
+        }
+    }
+}
